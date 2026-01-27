982c30c39956197852c0990d61ceb8ffbd986e88
xiaohajiayou <923390377@qq.com>
2026-01-27 19:17:26 +0800

    refactor: modify variables for attention

--- src/ops/self_attention/cpu/self_attention_cpu.cpp
+++ src/ops/self_attention/cpu/self_attention_cpu.cpp
@@ -12,48 +12,47 @@ namespace llaisys::ops::cpu {
 
 template<typename T>
 void self_attention_(
-    T* out,
-    const T* q,
-    const T* k,
-    const T* v,
-    size_t qlen,
+    T* out,       // [seqlen, nhead, head_kv_dim]
+    const T* q,   // [seqlen, nhead, head_dim]
+    const T* k,   // [kvlen, nkvhead, head_kv_dim]
+    const T* v,   // [kvlen, nkvhead, head_kv_dim]
+    size_t seqlen, 
     size_t kvlen,
     size_t nhead,
     size_t nkvhead,
-    size_t d,
-    size_t dv,
+    size_t head_dim,
     float scale)
 {
     size_t group_size = nhead / nkvhead;
 
-    std::vector<float> scores(kvlen);
+    std::vector<float> scores(kvlen);  
+    // 整体计算 QK^T 得分，维度为[seq_len * kv_len], 每轮计算一个 q head 的得分，维度为[kv_len]
 
-    for (size_t t = 0; t < qlen; ++t) {
-        for (size_t h = 0; h < nhead; ++h) {
+    for (size_t t = 0; t < seqlen; ++t) {
+        for (size_t q_h_idx = 0; q_h_idx < nhead; ++q_h_idx) {
 
-            size_t kv_h = h / group_size;
+            size_t kv_h_idx = q_h_idx / group_size;  // 计算每个 q head 对应的 kv head 索引
 
-            const T* q_ptr = q + (t * nhead + h) * d;
-            // const T* k_base = k + kv_h * d;
-            // const T* v_base = v + kv_h * dv;
-            T* out_ptr = out + (t * nhead + h) * dv;
+            const T* q_ptr = q + (t * nhead + q_h_idx) * head_dim;
+            T* out_ptr = out + (t * nhead + q_h_idx) * head_dim;
 
             // ---- 1. 计算 QK^T ----
-            for (size_t tk = 0; tk < kvlen; ++tk) {
-                ptrdiff_t offset = static_cast<ptrdiff_t>(kvlen) - static_cast<ptrdiff_t>(qlen);
-                if (static_cast<ptrdiff_t>(tk) > static_cast<ptrdiff_t>(t) + offset) {
-                    scores[tk] = -1e30f;
+            for (size_t i = 0; i < kvlen; ++i) {
+                ptrdiff_t offset = static_cast<ptrdiff_t>(kvlen) - static_cast<ptrdiff_t>(seqlen);
+                if (static_cast<ptrdiff_t>(i) > static_cast<ptrdiff_t>(t) + offset) {
+                    scores[i] = -1e30f;
                     continue;
                 }
                 
 
-                const T* k_ptr = k + (tk * nkvhead + kv_h) * d;
+                const T* k_ptr = k + (i * nkvhead + kv_h_idx) * head_dim;
+                //第i个时序位置，对应kv头的偏移位置
 
                 float dot = 0.f;
-                for (size_t i = 0; i < d; ++i) {
-                    dot += llaisys::utils::cast<float>(q_ptr[i]) * llaisys::utils::cast<float>(k_ptr[i]);
+                for (size_t j = 0; j < head_dim; ++j) {
+                    dot += llaisys::utils::cast<float>(q_ptr[j]) * llaisys::utils::cast<float>(k_ptr[j]);
                 }
-                scores[tk] = dot * scale;
+                scores[i] = dot * scale;
             }
 
             // ---- 2. softmax ----
@@ -71,11 +70,11 @@ void self_attention_(
                 scores[i] /= sum;
 
             // ---- 3. 加权 V ----
-            for (size_t i = 0; i < dv; ++i) {
+            for (size_t i = 0; i < head_dim; ++i) {
                 float acc = 0.f;
-                for (size_t tk = 0; tk < kvlen; ++tk) {
-                    const T* v_ptr = v + (tk * nkvhead + kv_h) * dv;
-                    acc += scores[tk] * llaisys::utils::cast<float>(v_ptr[i]);
+                for (size_t j = 0; j < kvlen; ++j) {
+                    const T* v_ptr = v + (j * nkvhead + kv_h_idx) * head_dim;
+                    acc += scores[j] * llaisys::utils::cast<float>(v_ptr[i]);
                 }
                 out_ptr[i] = llaisys::utils::cast<T>(acc);
             }
@@ -101,15 +100,16 @@ void self_attention(tensor_t attn_val,
 
     size_t seqlen   = q->shape()[0];
     size_t nhead    = q->shape()[1];
-    size_t d        = q->shape()[2];
+    size_t head_dim        = q->shape()[2];
 
     size_t total_len = k->shape()[0];
     size_t nkvhead   = k->shape()[1];
-    size_t dv        = v->shape()[2];
+    size_t head_kv_dim        = k->shape()[2];
 
+    ASSERT(head_dim == head_kv_dim, "head_dim must equal head_kv_dim");
     ASSERT(attn_val->shape()[0] == seqlen, "attn_val seqlen mismatch");
     ASSERT(attn_val->shape()[1] == nhead,  "attn_val head mismatch");
-    ASSERT(attn_val->shape()[2] == dv,     "attn_val dv mismatch");
+    ASSERT(attn_val->shape()[2] == head_dim,     "attn_val dv mismatch");
 
     switch (q->dtype()) {
     case LLAISYS_DTYPE_F32:
@@ -118,21 +118,21 @@ void self_attention(tensor_t attn_val,
             reinterpret_cast<const float*>(q->data()),
             reinterpret_cast<const float*>(k->data()),
             reinterpret_cast<const float*>(v->data()),
-            seqlen, total_len, nhead, nkvhead, d, dv, scale);
+            seqlen, total_len, nhead, nkvhead, head_dim, scale);
     case LLAISYS_DTYPE_BF16:
         return self_attention_(
             reinterpret_cast<llaisys::bf16_t*>(attn_val->data()),
             reinterpret_cast<const llaisys::bf16_t*>(q->data()),
             reinterpret_cast<const llaisys::bf16_t*>(k->data()),
             reinterpret_cast<const llaisys::bf16_t*>(v->data()),
-            seqlen, total_len, nhead, nkvhead, d, dv, scale);
+            seqlen, total_len, nhead, nkvhead, head_dim, scale);
     case LLAISYS_DTYPE_F16:
         return self_attention_(
             reinterpret_cast<llaisys::fp16_t*>(attn_val->data()),
             reinterpret_cast<const llaisys::fp16_t*>(q->data()),
             reinterpret_cast<const llaisys::fp16_t*>(k->data()),
             reinterpret_cast<const llaisys::fp16_t*>(v->data()),
-            seqlen, total_len, nhead, nkvhead, d, dv, scale);
+            seqlen, total_len, nhead, nkvhead, head_dim, scale);
     default:
         EXCEPTION_UNSUPPORTED_DATATYPE(q->dtype());
     }
